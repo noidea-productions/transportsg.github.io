bfa4eb16b68e594be2adcf4a1e5c6e62e8dd3dbe
Eyeball Edward <edwardyeung.private@gmail.com>
2018-03-14 18:01:00 +0800

    static 174

--- controllers/lecip.js
+++ controllers/lecip.js
@@ -1,6 +1,15 @@
+let currentSvc = '';
+
 function renderText(line1, line2) {
-    document.getElementById('output-line1').textContent = line1;
-    document.getElementById('output-line2').textContent = line2;
+    if (!!line1)
+        document.getElementById('output-line1').textContent = line1;
+    if (!!line2)
+        document.getElementById('output-line2').textContent = line2;
+}
+
+function runMainFirmware() {
+    renderText('Route No: 174  1', '             E21');
+    // renderText(Array(16).fill('a').join(''), Array(15).fill(' ').join('') + 'a');
 }
 
 function performStartup() {
@@ -15,6 +24,10 @@ function performStartup() {
             renderText(lines[0], lines[1]);
         }, index * 2000);
     });
+
+    setTimeout(() => {
+        runMainFirmware();
+    }, (textSets.length + 1) * 1000);
 }
 
 function main() {
