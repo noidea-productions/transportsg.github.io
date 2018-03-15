f345cc18be0a8c444504d92223ea70a691a23cbe
Eyeball Edward <edwardyeung.private@gmail.com>
2018-03-15 21:11:45 +0800

    some led tests

--- displays/lecip/rear.js
+++ displays/lecip/rear.js
@@ -9,11 +9,28 @@ window.addEventListener('load', () => {
             ledSegment.className = 'led-segment';
 
             let led = document.createElement('div');
-            led.className = 'led';
+            led.className = 'led led-off';
 
             ledSegment.appendChild(led);
 
             ledContainer.appendChild(ledSegment);
         }
     }
+
+    let delayTime = 50;
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < 40; x++) {
+            setTimeout(() => {
+                setLEDState(x, y, true);
+                setTimeout(() => {setLEDState(x, y, false)}, delayTime)
+            }, (((x * height) + y)) * delayTime);
+        }
+    }
 });
+
+function setLEDState(x, y, state) {
+    let led = document.querySelector('#led-' + x + '-' + y + ' > div.led');
+
+    var state = 'led-' + (state ? 'on' : 'off');
+    led.className = 'led ' + state;
+}
