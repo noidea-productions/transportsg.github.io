c95e872246905a6c02569248809fb37971bebc43
Eyeball Edward <edwardyeung.private@gmail.com>
2018-03-15 21:34:23 +0800

    simple show char code

--- displays/lecip/rear.js
+++ displays/lecip/rear.js
@@ -2,8 +2,8 @@ let width = 40, height = 20;
 
 window.addEventListener('load', () => {
     let ledContainer = document.getElementById('led-container');
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
             let ledSegment = document.createElement('div');
             ledSegment.id = 'led-' + x + '-' + y;
             ledSegment.className = 'led-segment';
@@ -17,20 +17,28 @@ window.addEventListener('load', () => {
         }
     }
 
-    let delayTime = 50;
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < 40; x++) {
-            setTimeout(() => {
-                setLEDState(x, y, true);
-                setTimeout(() => {setLEDState(x, y, false)}, delayTime)
-            }, (((x * height) + y)) * delayTime);
-        }
-    }
+    showChar(1, 'medium', 0);
 });
 
 function setLEDState(x, y, state) {
     let led = document.querySelector('#led-' + x + '-' + y + ' > div.led');
+    console.log('#led-' + x + '-' + y + ' > div.led')
 
     var state = 'led-' + (state ? 'on' : 'off');
     led.className = 'led ' + state;
+}
+
+function showChar(char, type, dx) {
+    if (!(type in charSet)) return;
+    if (!(char in charSet[type])) return;
+
+    let charData = charSet[type][char];
+
+    let charWidth = charData[0].length;
+
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < charWidth; x++) {
+            setLEDState(x + dx, y, charData[y][x]);
+        }
+    }
 }
