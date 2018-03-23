4bd4c912eb2baf6f78bad9d0e5a757e9f6e7bf55
Eyeball Edward <edwardyeung.private@gmail.com>
2018-03-23 13:46:58 +0800

    added 174 data

--- eds-data.js
+++ eds-data.js
@@ -16,6 +16,45 @@ let EDSData = {
             'Tampines Ave 7'
         ]
     },
+    174: {
+        1: [
+            'J. West Ctrl 3',
+            'Boon Lay Ave',
+            'Jurong Rd',
+            'Bt Batok W. Ave 3',
+            'Bt Batok E. Ave 3',
+            'Jln Jurong Kechil',
+            'Dunearn Rd',
+            'Farrer Rd',
+            'Holland Rd',
+            'Napier Rd',
+            'Orchard Rd',
+            'Bras Basah Rd',
+            'Nth Bridge Rd',
+            'Upp Cross St',
+            'Kg Bahru Rd',
+            'Spooner Rd'
+        ],
+        2: [
+            'Spooner Rd',
+            'Kg Bahru Rd',
+            'Eu Tong Sen St',
+            'Outram Rd',
+            'Hill St',
+            'Penang Rd',
+            'Orchard Blvd',
+            'Napier Rd',
+            'Holland Rd',
+            'Farrer Rd',
+            'Bt Timah Rd',
+            'Jln Jurong Kechil',
+            'Bt Batok E. Ave 6',
+            'Bt Batok W. Ave 3',
+            'Jurong Rd',
+            'Boon Lay Ave',
+            'Jurong W. Ctrl 3'
+        ]
+    },
     181: {
         1: [
             'J. West Ctrl 3',
