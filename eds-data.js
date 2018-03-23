69618922c927d27a490db280b5a0867369b4a856
Eyeball Edward <edwardyeung.private@gmail.com>
2018-03-23 18:05:37 +0800

    trim 174 data

--- eds-data.js
+++ eds-data.js
@@ -6,10 +6,8 @@ let EDSData = {
             'Tampines St 33',
             'Tampines Ave 2',
             'Bedok Resvr Rd',
-            'Bedok Nth Ave 3',
             'Bedok Nth Dr',
             'Bedok Nth Rd',
-            'Bedok Nth Ave 3',
             'Bedok Resvr Rd',
             'Tampines Ave 2',
             'Tampines St 33',
@@ -18,41 +16,23 @@ let EDSData = {
     },
     174: {
         1: [
-            'J. West Ctrl 3',
-            'Boon Lay Ave',
-            'Jurong Rd',
-            'Bt Batok W. Ave 3',
-            'Bt Batok E. Ave 3',
+            'Bt Batok Ave 6, 3',
             'Jln Jurong Kechil',
             'Dunearn Rd',
             'Farrer Rd',
             'Holland Rd',
-            'Napier Rd',
             'Orchard Rd',
             'Bras Basah Rd',
-            'Nth Bridge Rd',
-            'Upp Cross St',
-            'Kg Bahru Rd',
-            'Spooner Rd'
+            'Nth Bridge Rd'
         ],
         2: [
-            'Spooner Rd',
-            'Kg Bahru Rd',
             'Eu Tong Sen St',
-            'Outram Rd',
-            'Hill St',
-            'Penang Rd',
             'Orchard Blvd',
-            'Napier Rd',
             'Holland Rd',
             'Farrer Rd',
             'Bt Timah Rd',
             'Jln Jurong Kechil',
-            'Bt Batok E. Ave 6',
-            'Bt Batok W. Ave 3',
-            'Jurong Rd',
-            'Boon Lay Ave',
-            'Jurong W. Ctrl 3'
+            'Bt Batok Ave 6, 3'
         ]
     },
     181: {
