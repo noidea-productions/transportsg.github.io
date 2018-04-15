d8c5c93f2948977d28f9d7aa80443b6aeb8940a4
Eyeball Edward <edwardyeung.private@gmail.com>
2018-04-15 17:15:42 +0800

    more codes in

--- controllers/icu-data/smrt.js
+++ controllers/icu-data/smrt.js
@@ -26,5 +26,41 @@ let EDSData = {
     7: {
         renderType: 'fullSized',
         text: 'OFF SERVICE'
+    },
+    8: {
+        renderType: 'fullSized',
+        text: 'OFF SERVICE'
+    },
+    9: {
+        renderType: 'fullSized',
+        text: 'ON DRIVING TEST'
+    },
+    10: {
+        renderType: 'fullSized',
+        text: 'FERRY SERVICE'
+    },
+    11: {
+        renderType: 'fullSized',
+        text: 'MRT SHUTTLE'
+    },
+    13: {
+        renderType: 'fullSized',
+        text: 'TRAINING BUS'
+    },
+    14: {
+        renderType: 'fullSized',
+        text: '???'
+    },
+    16: {
+        renderType: 'fullSized',
+        text: 'PULAU TEKONG'
+    },
+    18: {
+        renderType: 'fullSized',
+        text: 'LRT SHUTTLE A'
+    },
+    19: {
+        renderType: 'fullSized',
+        text: 'LRT SHUTTLE B'
     }
 }
