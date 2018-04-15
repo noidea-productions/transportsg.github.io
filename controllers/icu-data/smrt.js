ebd28ab3df58ffa317902a05618499dbf8e803e8
Eyeball Edward <edwardyeung.private@gmail.com>
2018-04-15 17:02:42 +0800

    test data

---
+++ controllers/icu-data/smrt.js
@@ -0,0 +1,30 @@
+let data = {
+    1: {
+        renderType: 'fullSized',
+        text: 'CHARTERED'
+    },
+    2: {
+        renderType: 'fullSized',
+        text: 'ANG MO KIO DEPOT'
+    },
+    3: {
+        renderType: 'fullSized',
+        text: 'KRANJI DEPOT'
+    },
+    4: {
+        renderType: 'fullSized',
+        text: 'SMRT BUSES'
+    },
+    5: {
+        renderType: 'fullSized',
+        text: 'OFF SERVICE'
+    },
+    6: {
+        renderType: 'fullSized',
+        text: 'WOODLANDS DEPOT'
+    },
+    7: {
+        renderType: 'fullSized',
+        text: 'OFF SERVICE'
+    }
+}
